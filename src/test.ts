b5d451fdc954719d1c8633483d681d4a364ef35e
Tamim Iqbal Maruf <marufmd775@gmail.com>
2023-12-14 22:54:39 +0600

    practicing typeScript again

---
+++ src/test.ts
@@ -0,0 +1,27 @@
+type User = {
+    id: number,
+    name: string
+}
+
+const users: User[] = [
+    {
+        id: 12,
+        name: "Tamim"
+    },
+    {
+        id: 13,
+        name: "Iqbal"
+    },
+    {
+        id: 14,
+        name: "Maruf"
+    },
+];
+
+
+function getPropertyFromArray(){
+    
+}
+
+
+
